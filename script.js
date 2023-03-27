970f35d121ece2f2a4d0fe3de50a85ef64bad25b
Lucas Barboza <Okamiakume@gmail.com>
2023-03-27 20:36:07 -0300

    commit

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+  var loadedPages = {};
+
+  function loadPage(pageUrl) {
+    if (!loadedPages[pageUrl]) {
+      $.get(pageUrl, function(response) {
+        $('#content').html(response);
+        loadedPages[pageUrl] = response;
+      }).fail(function() {
+        $('#content').html("<p>Erro ao carregar página.</p>");
+      });
+    } else {
+      $('#content').html(loadedPages[pageUrl]);
+    }
+  }
+
+  $('#link-about').on('click', function(event) {
+    event.preventDefault();
+    loadPage('about.html');
+  });
+
+  $('#link-home').on('click', function(event) {
+    event.preventDefault();
+    loadPage('home.html');
+  });
+
+
+  $('#link-genres').on('click', function(event) {
+    event.preventDefault();
+    loadPage('genres.html');
+  });
+
+  $('#link-contact').on('click', function(event) {
+    event.preventDefault();
+    loadPage('contact.html');
+  });
+});
+
+
+
+function loadPage(pageUrl) {
+  if (!loadedPages[pageUrl]) {
+    $.get(pageUrl, function(response) {
+      $('#content').empty(); // Limpa o conteúdo do elemento #content
+      $('#content').html(response);
+      loadedPages[pageUrl] = true;
+    }).fail(function() {
+      $('#content').html("<p>Erro ao carregar página.</p>");
+    });
+  }
+}
